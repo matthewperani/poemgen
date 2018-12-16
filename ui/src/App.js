c4c63212a863b909af6a73543b67835754c7f032
Rosa <rosa.m.tung@gmail.com>
2018-12-16 10:52:17 -0500

    add haiku to front end

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,18 +5,28 @@ import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  //componentDidMount() {
-  //  axios.get('/hello')
-  //  .then( res => {
-  //    console.log(res.data);
-  //  });
-  //}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentHaiku: ""
+    };
+  }
+
+  _getHaiku() {
+    axios.get("http://192.168.99.100:5000/haiku").then(res => {
+      this.setState({
+        currentHaiku: res.data
+      });
+      console.log("haiku: ", res.data);
+    });
+  }
 
   render() {
     return (
       <div className="App">
         <h1> Random Poem Generator! </h1>
-        <button> I want a random Poem </button>
+        <button onClick={this._getHaiku}> I want a random Haiku </button>
+        <div>{this.state.currentHauku}</div>
       </div>
     );
   }
